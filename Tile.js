08d8c0a7413a9b5ea3fa2c34531a6ef3299fe338
practicasfront <mario.r.armas@gmail.com>
2022-04-27 05:22:35 -0600

    Update script

--- Tile.js
+++ Tile.js
@@ -37,4 +37,15 @@ export default class Tile {
   remove() {
     this.#tileElement.remove()
   }
+
+  waitForTransition(animation = false) {
+    return new Promise(resolve => {
+      this.#tileElement.addEventListener(
+        animation ? 'animationend' : 'transitionend',
+        resolve, {
+          once: true
+        }
+      )
+    })
+  }
 }
